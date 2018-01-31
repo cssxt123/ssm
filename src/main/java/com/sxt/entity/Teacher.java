b301f7d59e337f00b628fac2e8f9653ba0198356
cssxt123 <2637181042@qq.com>
2018-01-31 14:56:37 +0800

    添加了sayHello()方法

--- src/main/java/com/sxt/entity/Teacher.java
+++ src/main/java/com/sxt/entity/Teacher.java
@@ -3,4 +3,8 @@ package com.sxt.entity;
 public class Teacher{
     private int teaId;
     private String teaName;
+    
+    public void sayHello(){
+    	System.out.println("工号："+teaId+",姓名："+teaName);
+    }
 }
